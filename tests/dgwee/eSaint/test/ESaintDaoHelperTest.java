fe7abfe4935a23975b0c8ae5e406233da9aaeb53
Andrew Zurn <awzurn@csbsju.edu>
2013-12-03 22:20:00 -0600

    Added needed tests in DaoHelperTest
    Tests need to be implemented

--- tests/dgwee/eSaint/test/ESaintDaoHelperTest.java
+++ tests/dgwee/eSaint/test/ESaintDaoHelperTest.java
@@ -7,8 +7,108 @@ import org.junit.Test;
 public class ESaintDaoHelperTest {
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void testLoginAdmin(){
+	
+    }
+    
+    @Test
+    public void testLoginUser(){
+	
+    }
+    
+    @Test
+    public void testGetAllItems(){
+	
+    }
+    
+    @Test
+    public void testGetItem(){
+	
+    }
+    
+    @Test
+    public void testGetItemBidders(){
+	
+    }
+    
+    @Test
+    public void testSearchItems(){
+	
+    }
+    
+    @Test
+    public void testGetItemsBidOn(){
+	
     }
 
+    @Test
+    public void testGetItemsSold(){
+	
+    }
+    
+    @Test
+    public void testGetItemsWon(){
+	
+    }
+    
+    @Test
+    public void testGetAllUsers(){
+	
+    }
+    
+    @Test 
+    public void testInsertUser(){
+	
+    }
+    
+    @Test
+    public void testUpdateUser(){
+	
+    }
+    
+    @Test
+    public void testGetComissionReport(){
+	
+    }
+    
+    @Test
+    public void testGetSalesReport(){
+	
+    }
+    
+    @Test
+    public void testInsertItem(){
+	
+    }
+    
+    @Test
+    public void testUpdateItem(){
+	
+    }
+    
+    @Test
+    public void testInsertAuctionBid(){
+	
+    }
+    
+    @Test
+    public void testInsertBuyerFeedback(){
+	
+    }
+    
+    @Test
+    public void testInsertSellerFeedback(){
+	
+    }
+    
+    @Test
+    public void testGetBuyerFeedback(){
+	
+    }
+    
+    @Test
+    public void testGetSellerFeedback(){
+	
+    }
+    
 }
